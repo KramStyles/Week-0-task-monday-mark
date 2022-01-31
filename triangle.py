2e6f7a914b79a715db2a9b93ea17a63c80b7eb37
kramstyles <mark.eke@decagon.dev>
2022-01-31 18:29:29 +0100

    Centeres the triangle

--- triangle.py
+++ triangle.py
@@ -9,12 +9,15 @@ def inverted_triangle(number):
     if type(number) == int:
         # Making sure number is an integer
         multiplier = (number - 1) * 2
-        for x in range(multiplier, 0, -2):
-            print((" * " * x)[:-2] + "\n")
+        # Multiplier stores the double of the inputted number (less of one before multipliation)
+        for x in range(multiplier, 0, -2): # For loop to print the numbers in reverse
+
+            # Prints out the asterisx character at the center of the terminal (Couldn't get the exact center of the terminal because no libraries were imported)
+            print("\n", (" * " * x)[:-2].center(100))
     else:
+        # Just in case the input is not a number
         print("Input must be a valid number")
 
 
 
 inverted_triangle(6)
-print("abcde"[:-1])
