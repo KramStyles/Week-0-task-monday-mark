0ed869eac651e26782d5fafcdd60e09e38a57559
kramstyles <mark.eke@decagon.dev>
2022-01-31 18:41:39 +0100

    Added comments for leap year

--- leap_year.py
+++ leap_year.py
@@ -5,10 +5,13 @@ def leap_year(year):
         return: result(boolean) - True or False for Leap and non leap years respectively 
         """
         if not year % 4 and str(year)[-2:] != "00":
+            # Checks if the year is divisible by 4 and not a century year
             return True
         elif str(year)[-2:] == "00" and year% 400 == 0:
+            # Returns true if it's a century year divisible by 400
             return True
         else:
+            # Returns false if it's not a leap year
             return False
 
 print(leap_year(1984))
