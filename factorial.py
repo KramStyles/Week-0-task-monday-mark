f6a8e223871f38b8a4dbbfb5c683ddae5874b3ae
kramstyles <mark.eke@decagon.dev>
2022-01-31 18:33:05 +0100

    Added comments to factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,14 @@
 def factorial(number):
+    """
+    A function that returns the factorial of any number given to it
+    param: number(int) - Number inputted by the user
+    return: (str) - A string suggesting the factorial of the given number
+    """
+
+    # Ans variable to hold our answers during calculation
     ans = 1
     for x in range(1, number+1):
+        # Multiplies the current index with the existing value of ans
         ans *= x
     return(f"The factorial of {number} is {ans}.")
 
